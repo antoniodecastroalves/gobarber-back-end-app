e3c4713461dd3c56b6063361bbe4fb97a0403f3e
Antonio Alves <antoniodecastroalves@gmail.com>
2020-08-13 17:17:04 -0300

    Configuring Imports

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,11 +4,11 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
+import uploadConfig from '@config/upload';
 import routes from './routes';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import AppError from '@shared/errors/AppError';
 
-import './database/index';
+import '@shared/infra/typeorm';
 
 const app = express();
 
