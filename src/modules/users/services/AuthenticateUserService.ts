7e92bc415d9f9191029e081640926c2a3270e770
Antonio Alves <antoniodecastroalves@gmail.com>
2020-08-15 11:49:07 -0300

    Dependency injection

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,9 +1,10 @@
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import authConfig from '@config/auth';
+import { injectable, inject } from 'tsyringe';
 
-import IUsersRepository from '../repositories/IUsersRepository';
 import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
 
 import User from '../infra/typeorm/entities/User';
 
@@ -17,8 +18,12 @@ interface IResponse {
   token: string;
 }
 
+@injectable()
 class AuthenticateUserService {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
 
   public async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.usersRepository.findByEmail(email);
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,10 +5,11 @@ import cors from 'cors';
 import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
-import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import routes from './routes';
 
 import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
